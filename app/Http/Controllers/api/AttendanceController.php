742349d2c935763348591e99d13ac9832a33c4ec
abbasanwar158 <abbas.anwar@devbox.co>
2022-09-16 17:30:47 +0500

    post request

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -19,15 +19,16 @@ class AttendanceController extends Controller
 
     public function print()
     {
-        $path_for_file = '/home/abbasanwar/Devbox/time-attendance-lr/public/uploads/';
+        $path_for_file = 'F:/auto-print-application/public/uploads/';
         $ids = array();
         $response = Http::get('https://gettabox.channeldispatch.co.uk/api/v1/download_file?user_id=7');
         foreach ($response['files'] as $key => $value) {
           $data = base64_decode($value[0]);
           echo file_put_contents("$path_for_file"."$value[1].pdf", $data);
           array_push($ids, $value[2]);
-
         }
-
+        $res = Http::post("https://gettabox.channeldispatch.co.uk/api/v1/update_printers", [
+          'printer_label_ids' => $ids
+        ]);
     }
 }
