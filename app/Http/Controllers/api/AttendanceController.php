8699a75d14e00fa81de34791eab72550b09b01b9
abbasanwar158 <abbas.anwar@devbox.co>
2022-11-23 15:21:47 +0500

    while loop 5 sec

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -22,15 +22,21 @@ class AttendanceController extends Controller
     {
       $path_for_file = 'F:/auto-print-application/public/uploads/';
       $user_pin = File::get(public_path("users.log"));
-      $ids = array();
-      $response = Http::get("https://gettabox.channeldispatch.co.uk/api/v1/download_file?user_id=$user_pin");
-      foreach ($response['files'] as $key => $value) {
-        $data = base64_decode($value[0]);
-        echo file_put_contents("$path_for_file"."$value[1].pdf", $data);
-        array_push($ids, $value[2]);
+      $current_sec = date('s', time());
+      $new_sec = $current_sec;
+      while ($new_sec + 5 != $current_sec) {
+        $new_sec = date('s', time());
+        $ids = array();
+        $response = Http::get("https://gettabox.channeldispatch.co.uk/api/v1/download_file?user_id=$user_pin");
+        foreach ($response['files'] as $key => $value) {
+          $data = base64_decode($value[0]);
+          echo file_put_contents("$path_for_file"."$value[1].pdf", $data);
+          array_push($ids, $value[2]);
+        }
+        $res = Http::post("https://gettabox.channeldispatch.co.uk/api/v1/update_printers", [
+          'printer_label_ids' => $ids
+        ]);
+        sleep(5);
       }
-      $res = Http::post("https://gettabox.channeldispatch.co.uk/api/v1/update_printers", [
-        'printer_label_ids' => $ids
-      ]);
     }
 }
