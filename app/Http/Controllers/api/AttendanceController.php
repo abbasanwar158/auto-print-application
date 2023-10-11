69c8b23f72f2c3a1dcb2c8316f683e34cc9bb717
abbasanwar158 <abbas.anwar@devbox.co>
2023-10-11 17:12:22 +0500

    if check of present file

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers\api;
-
 use App\Models\TblAttendance;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -17,7 +15,6 @@ class AttendanceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-
     public function print()
     {
       $path_for_file = 'F:/auto-print-application/public/uploads/';
@@ -29,13 +26,13 @@ class AttendanceController extends Controller
         $ids = array();
         $response = Http::get("https://gettabox.channeldispatch.co.uk/api/v1/download_file?user_id=$user_pin");
         foreach ($response['files'] as $key => $value) {
-          $data = base64_decode($value[0]);
-          echo file_put_contents("$path_for_file"."$value[1].pdf", $data);
-          array_push($ids, $value[2]);
+          $fileName = "$path_for_file"."$value[1].pdf";
+          if (!file_exists($fileName)) {
+            $data = base64_decode($value[0]);
+            echo file_put_contents($fileName, $data);
+            array_push($ids, $value[2]);
+          }
         }
-        $res = Http::post("https://gettabox.channeldispatch.co.uk/api/v1/update_printers", [
-          'printer_label_ids' => $ids
-        ]);
         sleep(5);
       }
     }
